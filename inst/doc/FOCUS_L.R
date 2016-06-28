f0fb3448958dcf608a9fb3f11fcbe799e19348b5
Johannes Ranke <jranke@uni-bremen.de>
2016-06-28 07:45:28 +0000

    version 0.9.43

--- inst/doc/FOCUS_L.R
+++ inst/doc/FOCUS_L.R
@@ -3,7 +3,7 @@ library(knitr)
 opts_chunk$set(tidy = FALSE, cache = TRUE)
 
 ## ------------------------------------------------------------------------
-library("mkin")
+library("mkin", quietly = TRUE)
 FOCUS_2006_L1 = data.frame(
   t = rep(c(0, 1, 2, 3, 5, 7, 14, 21, 30), each = 2),
   parent = c(88.3, 91.4, 85.6, 84.5, 78.9, 77.6, 
@@ -12,17 +12,18 @@ FOCUS_2006_L1 = data.frame(
 FOCUS_2006_L1_mkin <- mkin_wide_to_long(FOCUS_2006_L1)
 
 ## ------------------------------------------------------------------------
-m.L1.SFO <- mkinfit("SFO", FOCUS_2006_L1_mkin, quiet=TRUE)
+m.L1.SFO <- mkinfit("SFO", FOCUS_2006_L1_mkin, quiet = TRUE)
 summary(m.L1.SFO)
 
-## ----fig.width=7, fig.height = 5-----------------------------------------
-plot(m.L1.SFO)
+## ----fig.width = 6, fig.height = 5---------------------------------------
+plot(m.L1.SFO, show_errmin = TRUE, main = "FOCUS L1 - SFO")
 
-## ----fig.width=7, fig.height = 5-----------------------------------------
+## ----fig.width = 6, fig.height = 5---------------------------------------
 mkinresplot(m.L1.SFO, ylab = "Observed", xlab = "Time")
 
-## ------------------------------------------------------------------------
+## ----fig.width = 6, fig.height = 5---------------------------------------
 m.L1.FOMC <- mkinfit("FOMC", FOCUS_2006_L1_mkin, quiet=TRUE)
+plot(m.L1.FOMC, show_errmin = TRUE, main = "FOCUS L1 - FOMC")
 summary(m.L1.FOMC, data = FALSE)
 
 ## ------------------------------------------------------------------------
@@ -33,31 +34,21 @@ FOCUS_2006_L2 = data.frame(
              2.6, 1.2, 0.3, 0.6))
 FOCUS_2006_L2_mkin <- mkin_wide_to_long(FOCUS_2006_L2)
 
-## ------------------------------------------------------------------------
+## ----fig.width = 7, fig.height = 6---------------------------------------
 m.L2.SFO <- mkinfit("SFO", FOCUS_2006_L2_mkin, quiet=TRUE)
-summary(m.L2.SFO)
+plot(m.L2.SFO, show_residuals = TRUE, show_errmin = TRUE, 
+     main = "FOCUS L2 - SFO")
 
-## ----fig.height = 8------------------------------------------------------
-par(mfrow = c(2, 1))
-plot(m.L2.SFO)
-mkinresplot(m.L2.SFO)
-
-## ----fig.height = 8------------------------------------------------------
+## ----fig.width = 7, fig.height = 6---------------------------------------
 m.L2.FOMC <- mkinfit("FOMC", FOCUS_2006_L2_mkin, quiet = TRUE)
-par(mfrow = c(2, 1))
-plot(m.L2.FOMC)
-mkinresplot(m.L2.FOMC)
+plot(m.L2.FOMC, show_residuals = TRUE,
+     main = "FOCUS L2 - FOMC")
 summary(m.L2.FOMC, data = FALSE)
 
-## ----fig.height = 5------------------------------------------------------
+## ----fig.width = 7, fig.height = 6---------------------------------------
 m.L2.DFOP <- mkinfit("DFOP", FOCUS_2006_L2_mkin, quiet = TRUE)
-plot(m.L2.DFOP)
-
-## ----fig.height = 5------------------------------------------------------
-m.L2.DFOP <- mkinfit("DFOP", FOCUS_2006_L2_mkin, 
-  parms.ini = c(k1 = 1, k2 = 0.01, g = 0.8),
-  quiet=TRUE)
-plot(m.L2.DFOP)
+plot(m.L2.DFOP, show_residuals = TRUE, show_errmin = TRUE,
+     main = "FOCUS L2 - DFOP")
 summary(m.L2.DFOP, data = FALSE)
 
 ## ------------------------------------------------------------------------
@@ -66,20 +57,15 @@ FOCUS_2006_L3 = data.frame(
   parent = c(97.8, 60, 51, 43, 35, 22, 15, 12))
 FOCUS_2006_L3_mkin <- mkin_wide_to_long(FOCUS_2006_L3)
 
-## ----fig.height = 5------------------------------------------------------
-m.L3.SFO <- mkinfit("SFO", FOCUS_2006_L3_mkin, quiet = TRUE)
-plot(m.L3.SFO)
-summary(m.L3.SFO)
+## ----fig.height = 8------------------------------------------------------
+# Only use one core here, not to offend the CRAN checks
+mm.L3 <- mmkin(c("SFO", "FOMC", "DFOP"), cores = 1,
+               list("FOCUS L3" = FOCUS_2006_L3_mkin), quiet = TRUE)
+plot(mm.L3)
 
 ## ----fig.height = 5------------------------------------------------------
-m.L3.FOMC <- mkinfit("FOMC", FOCUS_2006_L3_mkin, quiet = TRUE)
-plot(m.L3.FOMC)
-summary(m.L3.FOMC, data = FALSE)
-
-## ----fig.height = 5------------------------------------------------------
-m.L3.DFOP <- mkinfit("DFOP", FOCUS_2006_L3_mkin, quiet = TRUE)
-plot(m.L3.DFOP)
-summary(m.L3.DFOP, data = FALSE)
+summary(mm.L3[["DFOP", 1]])
+plot(mm.L3[["DFOP", 1]], show_errmin = TRUE)
 
 ## ------------------------------------------------------------------------
 FOCUS_2006_L4 = data.frame(
@@ -87,13 +73,14 @@ FOCUS_2006_L4 = data.frame(
   parent = c(96.6, 96.3, 94.3, 88.8, 74.9, 59.9, 53.5, 49.0))
 FOCUS_2006_L4_mkin <- mkin_wide_to_long(FOCUS_2006_L4)
 
-## ----fig.height = 5------------------------------------------------------
-m.L4.SFO <- mkinfit("SFO", FOCUS_2006_L4_mkin, quiet = TRUE)
-plot(m.L4.SFO)
-summary(m.L4.SFO, data = FALSE)
+## ----fig.height = 6------------------------------------------------------
+# Only use one core here, not to offend the CRAN checks
+mm.L4 <- mmkin(c("SFO", "FOMC"), cores = 1,
+               list("FOCUS L4" = FOCUS_2006_L4_mkin), 
+               quiet = TRUE)
+plot(mm.L4)
 
-## ----fig.height = 5------------------------------------------------------
-m.L4.FOMC <- mkinfit("FOMC", FOCUS_2006_L4_mkin, quiet = TRUE)
-plot(m.L4.FOMC)
-summary(m.L4.FOMC, data = FALSE)
+## ----fig.height = 8------------------------------------------------------
+summary(mm.L4[["SFO", 1]], data = FALSE)
+summary(mm.L4[["FOMC", 1]], data = FALSE)
 
--- inst/doc/FOCUS_Z.R
+++ inst/doc/FOCUS_Z.R
@@ -20,124 +20,96 @@ FOCUS_2006_Z = data.frame(
 
 FOCUS_2006_Z_mkin <- mkin_wide_to_long(FOCUS_2006_Z)
 
-## ----FOCUS_2006_Z_fits_1, echo=TRUE, fig.height=4-------------------
-Z.2a <- mkinmod(Z0 = list(type = "SFO", to = "Z1"),
-                Z1 = list(type = "SFO"))
+## ----FOCUS_2006_Z_fits_1, echo=TRUE, fig.height=6-------------------
+Z.2a <- mkinmod(Z0 = mkinsub("SFO", "Z1"),
+                Z1 = mkinsub("SFO"))
 m.Z.2a <- mkinfit(Z.2a, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.2a)
-summary(m.Z.2a, data = FALSE)
+plot_sep(m.Z.2a)
+summary(m.Z.2a, data = FALSE)$bpar
 
-## ----FOCUS_2006_Z_fits_2, echo=TRUE, fig.height=4-------------------
-Z.2a.ff <- mkinmod(Z0 = list(type = "SFO", to = "Z1"),
-                   Z1 = list(type = "SFO"),
+## ----FOCUS_2006_Z_fits_2, echo=TRUE, fig.height=6-------------------
+Z.2a.ff <- mkinmod(Z0 = mkinsub("SFO", "Z1"),
+                   Z1 = mkinsub("SFO"),
                    use_of_ff = "max")
 
 m.Z.2a.ff <- mkinfit(Z.2a.ff, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.2a.ff)
-summary(m.Z.2a.ff, data = FALSE)
+plot_sep(m.Z.2a.ff)
+summary(m.Z.2a.ff, data = FALSE)$bpar
 
-## ----FOCUS_2006_Z_fits_3, echo=TRUE, fig.height=4-------------------
-Z.3 <- mkinmod(Z0 = list(type = "SFO", to = "Z1", sink = FALSE),
-               Z1 = list(type = "SFO"), use_of_ff = "max")
+## ----FOCUS_2006_Z_fits_3, echo=TRUE, fig.height=6-------------------
+Z.3 <- mkinmod(Z0 = mkinsub("SFO", "Z1", sink = FALSE),
+               Z1 = mkinsub("SFO"), use_of_ff = "max")
 m.Z.3 <- mkinfit(Z.3, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.3)
-summary(m.Z.3, data = FALSE)
+plot_sep(m.Z.3)
+summary(m.Z.3, data = FALSE)$bpar
 
-## ----FOCUS_2006_Z_fits_5, echo=TRUE, fig.height=4-------------------
-Z.5 <- mkinmod(Z0 = list(type = "SFO", to = "Z1", sink = FALSE),
-               Z1 = list(type = "SFO", to = "Z2", sink = FALSE),
-               Z2 = list(type = "SFO"))
+## ----FOCUS_2006_Z_fits_5, echo=TRUE, fig.height=7-------------------
+Z.5 <- mkinmod(Z0 = mkinsub("SFO", "Z1", sink = FALSE),
+               Z1 = mkinsub("SFO", "Z2", sink = FALSE),
+               Z2 = mkinsub("SFO"), use_of_ff = "max")
 m.Z.5 <- mkinfit(Z.5, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.5)
-summary(m.Z.5, data = FALSE)
+plot_sep(m.Z.5)
 
-## ----FOCUS_2006_Z_fits_6, echo=TRUE, fig.height=4-------------------
-Z.FOCUS <- mkinmod(Z0 = list(type = "SFO", to = "Z1", sink = FALSE),
-                   Z1 = list(type = "SFO", to = "Z2", sink = FALSE),
-                   Z2 = list(type = "SFO", to = "Z3"),
-                   Z3 = list(type = "SFO"))
-m.Z.FOCUS <- mkinfit(Z.FOCUS, FOCUS_2006_Z_mkin, 
-                     quiet = TRUE)
-plot(m.Z.FOCUS)
-summary(m.Z.FOCUS, data = FALSE)
-
-## ----FOCUS_2006_Z_residuals_6, echo=TRUE----------------------------
-par(mfrow = c(2, 2))
-mkinresplot(m.Z.FOCUS, "Z0", lpos = "bottomright")
-mkinresplot(m.Z.FOCUS, "Z1", lpos = "bottomright")
-mkinresplot(m.Z.FOCUS, "Z2", lpos = "bottomright")
-mkinresplot(m.Z.FOCUS, "Z3", lpos = "bottomright")
-
-## ----FOCUS_2006_Z_fits_6_ff, echo=TRUE, fig.height=4----------------
-Z.FOCUS.ff <- mkinmod(Z0 = list(type = "SFO", to = "Z1", sink = FALSE),
-                   Z1 = list(type = "SFO", to = "Z2", sink = FALSE),
-                   Z2 = list(type = "SFO", to = "Z3"),
-                   Z3 = list(type = "SFO"), 
+## ----FOCUS_2006_Z_fits_6, echo=TRUE, fig.height=8-------------------
+Z.FOCUS <- mkinmod(Z0 = mkinsub("SFO", "Z1", sink = FALSE),
+                   Z1 = mkinsub("SFO", "Z2", sink = FALSE),
+                   Z2 = mkinsub("SFO", "Z3"),
+                   Z3 = mkinsub("SFO"), 
                    use_of_ff = "max")
-m.Z.FOCUS.ff <- mkinfit(Z.FOCUS.ff, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.FOCUS.ff)
-summary(m.Z.FOCUS.ff, data = FALSE)
+m.Z.FOCUS <- mkinfit(Z.FOCUS, FOCUS_2006_Z_mkin, 
+                     parms.ini = m.Z.5$bparms.ode,
+                     quiet = TRUE)
+plot_sep(m.Z.FOCUS)
+summary(m.Z.FOCUS, data = FALSE)$bpar
+endpoints(m.Z.FOCUS)
 
-## ----FOCUS_2006_Z_fits_7, echo=TRUE, fig.height=4-------------------
-Z.mkin.1 <- mkinmod(Z0 = list(type = "SFO", to = "Z1", sink = FALSE),
-                    Z1 = list(type = "SFO", to = "Z2", sink = FALSE),
-                    Z2 = list(type = "SFO", to = "Z3"),
-                    Z3 = list(type = "SFORB"))
+## ----FOCUS_2006_Z_fits_7, echo=TRUE, fig.height=8-------------------
+Z.mkin.1 <- mkinmod(Z0 = mkinsub("SFO", "Z1", sink = FALSE),
+                    Z1 = mkinsub("SFO", "Z2", sink = FALSE),
+                    Z2 = mkinsub("SFO", "Z3"),
+                    Z3 = mkinsub("SFORB"))
 m.Z.mkin.1 <- mkinfit(Z.mkin.1, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.mkin.1)
-summary(m.Z.mkin.1, data = FALSE)
+plot_sep(m.Z.mkin.1)
+summary(m.Z.mkin.1, data = FALSE)$cov.unscaled
 
-## ----FOCUS_2006_Z_fits_8, echo=TRUE, fig.height=4-------------------
-Z.mkin.2 <- mkinmod(Z0 = list(type = "SFORB", to = "Z1", sink = FALSE),
-                    Z1 = list(type = "SFO"))
-m.Z.mkin.2 <- mkinfit(Z.mkin.2, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.mkin.2)
-summary(m.Z.mkin.2, data = FALSE)
-
-## ----FOCUS_2006_Z_fits_9, echo=TRUE, fig.height=4-------------------
-Z.mkin.3 <- mkinmod(Z0 = list(type = "SFORB", to = "Z1", sink = FALSE),
-                    Z1 = list(type = "SFO", to = "Z2", sink = FALSE),
-                    Z2 = list(type = "SFO"))
+## ----FOCUS_2006_Z_fits_9, echo=TRUE, fig.height=8-------------------
+Z.mkin.3 <- mkinmod(Z0 = mkinsub("SFORB", "Z1", sink = FALSE),
+                    Z1 = mkinsub("SFO", "Z2", sink = FALSE),
+                    Z2 = mkinsub("SFO"))
 m.Z.mkin.3 <- mkinfit(Z.mkin.3, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.mkin.3)
-summary(m.Z.mkin.3, data = FALSE)
+plot_sep(m.Z.mkin.3)
 
-## ----FOCUS_2006_Z_fits_10, echo=TRUE, fig.height=4------------------
-Z.mkin.4 <- mkinmod(Z0 = list(type = "SFORB", to = "Z1", sink = FALSE),
-                    Z1 = list(type = "SFO", to = "Z2", sink = FALSE),
-                    Z2 = list(type = "SFO", to = "Z3"),
-                    Z3 = list(type = "SFO"))
+## ----FOCUS_2006_Z_fits_10, echo=TRUE, fig.height=8------------------
+Z.mkin.4 <- mkinmod(Z0 = mkinsub("SFORB", "Z1", sink = FALSE),
+                    Z1 = mkinsub("SFO", "Z2", sink = FALSE),
+                    Z2 = mkinsub("SFO", "Z3"),
+                    Z3 = mkinsub("SFO"))
 m.Z.mkin.4 <- mkinfit(Z.mkin.4, FOCUS_2006_Z_mkin, 
+                      parms.ini = m.Z.mkin.3$bparms.ode,
                       quiet = TRUE)
-plot(m.Z.mkin.4)
-summary(m.Z.mkin.4, data = FALSE)
+plot_sep(m.Z.mkin.4)
 
-## ----FOCUS_2006_Z_fits_11, echo=TRUE, fig.height=4------------------
-Z.mkin.5 <- mkinmod(Z0 = list(type = "SFORB", to = "Z1", sink = FALSE),
-                    Z1 = list(type = "SFO", to = "Z2", sink = FALSE),
-                    Z2 = list(type = "SFO", to = "Z3"),
-                    Z3 = list(type = "SFORB"))
-m.Z.mkin.5 <- mkinfit(Z.mkin.5, FOCUS_2006_Z_mkin, quiet = TRUE)
-plot(m.Z.mkin.5)
-summary(m.Z.mkin.5, data = FALSE)$bpar
+## ----FOCUS_2006_Z_fits_11, echo=TRUE, fig.height=8------------------
+Z.mkin.5 <- mkinmod(Z0 = mkinsub("SFORB", "Z1", sink = FALSE),
+                    Z1 = mkinsub("SFO", "Z2", sink = FALSE),
+                    Z2 = mkinsub("SFO", "Z3"),
+                    Z3 = mkinsub("SFORB"))
+m.Z.mkin.5 <- mkinfit(Z.mkin.5, FOCUS_2006_Z_mkin, 
+                      parms.ini = m.Z.mkin.4$bparms.ode[1:4],
+                      quiet = TRUE)
+plot_sep(m.Z.mkin.5)
 
 ## ----FOCUS_2006_Z_fits_11a, echo=TRUE-------------------------------
 m.Z.mkin.5a <- mkinfit(Z.mkin.5, FOCUS_2006_Z_mkin, 
-                       parms.ini = c(k_Z3_bound_free = 0),
+                       parms.ini = c(m.Z.mkin.5$bparms.ode[1:7],
+                                     k_Z3_bound_free = 0),
                        fixed_parms = "k_Z3_bound_free",
                        quiet = TRUE)
-summary(m.Z.mkin.5a, data = FALSE)$bpar
+plot_sep(m.Z.mkin.5a)
 
 ## ----FOCUS_2006_Z_fits_11b, echo=TRUE-------------------------------
 mkinparplot(m.Z.mkin.5a)
 
 ## ----FOCUS_2006_Z_fits_11b_endpoints, echo=TRUE---------------------
 endpoints(m.Z.mkin.5a)
-
-## ----FOCUS_2006_Z_residuals_11--------------------------------------
-par(mfrow = c(2, 2))
-mkinresplot(m.Z.mkin.5, "Z0", lpos = "bottomright")
-mkinresplot(m.Z.mkin.5, "Z1", lpos = "bottomright")
-mkinresplot(m.Z.mkin.5, "Z2", lpos = "bottomright")
-mkinresplot(m.Z.mkin.5, "Z3", lpos = "bottomright")
 
